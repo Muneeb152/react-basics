4a43bf1a864ff463633d47cde01db91925442372
muneeb_akhtar <muneeb@superfuture.world>
2023-05-20 17:07:21 +0400

    Changes

--- src/App.js
+++ src/App.js
@@ -3,13 +3,20 @@ import './App.css';
 import Header from "./MyComponents/Header"
 import {Todos} from "./MyComponents/Todos"
 import {Footer} from "./MyComponents/Footer"
+import React, { useState } from 'react'
 
 function App() {
   const onDelete=(todo)=>{
-    console.log("I'm on Delete of todo:"+todo)
-  }
+    console.log("I'm on Delete of todo:"+todo);
+    //Deleting in this way in react doesnot work
+    // let index=todos.indexOf(todo);
+    // todos.splice(index,1);
 
-  let todos=[
+    setTodos(todos.filter((e)=>{
+      return e!==todo;
+    }));
+    }
+  const [todos, setTodos]=useState([
     {
       sno:1,
       title:"Go to the market",
@@ -24,9 +31,9 @@ function App() {
       sno:3,
       title:"Go to the ghat",
       desc:"You need to go to the ghat to get this job done3"
-    }
-  ]
+    }])
   return (
+    
     <>
       <Header title="My Todos List" searchBar={false} />
       <Todos todos={todos} onDelete={onDelete} />
