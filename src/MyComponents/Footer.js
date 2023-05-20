d3c3cd28d585cc96b9ff920c58daa04733ab5e6f
muneeb_akhtar <muneeb@superfuture.world>
2023-05-20 17:44:50 +0400

    Footer added

--- src/MyComponents/Footer.js
+++ src/MyComponents/Footer.js
@@ -1,10 +1,14 @@
-import React from 'react'
+import React from "react";
 
 export const Footer = () => {
+ let footerStyle={
+    position:"absolute",
+    top:"100vh",
+    width:"100%"
+  }
   return (
-    <div>
-      Footer Works
-    </div>
-  )
-}
-
+    <footer className="bg-dark text-light py-3" style={footerStyle}>
+      <p className="text-center">Copy &copy; MyTodosList.com</p>
+    </footer>
+  );
+};
