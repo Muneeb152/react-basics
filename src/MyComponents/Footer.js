519a7ea362223b1c9c88db44770fce8d3eb27019
muneeb_akhtar <muneeb@superfuture.world>
2023-05-20 17:56:51 +0400

    Changes

--- src/MyComponents/Footer.js
+++ src/MyComponents/Footer.js
@@ -1,10 +1,12 @@
 import React from "react";
+import "./footer.css"
 
 export const Footer = () => {
  let footerStyle={
     position:"absolute",
     top:"100vh",
-    width:"100%"
+    width:"100%",
+    // border:"2px solid red"
   }
   return (
     <footer className="bg-dark text-light py-3" style={footerStyle}>
