d9c2e3769fa50825aa9f0de6f2b8235d0803f327
muneeb_akhtar <muneeb@superfuture.world>
2023-05-22 17:13:18 +0400

    Changes

--- src/App.js
+++ src/App.js
@@ -4,9 +4,15 @@ import Header from "./MyComponents/Header";
 import { Todos } from "./MyComponents/Todos";
 import { AddTodo } from "./MyComponents/AddTodo";
 import { Footer } from "./MyComponents/Footer";
-import React, { useState } from "react";
+import React, { useEffect, useState, userEffect } from "react";
 
 function App() {
+  let initTodo;
+  if (localStorage.getItem("todos")=== null) {
+    initTodo = [];
+  } else {
+    initTodo = JSON.parse(localStorage.getItem("todos"));
+  }
   const onDelete = (todo) => {
     console.log("I'm on Delete of todo:" + todo);
     //Deleting in this way in react doesnot work
@@ -18,28 +24,32 @@ function App() {
         return e !== todo;
       })
     );
+    console.log("Deleted")
+    localStorage.setItem("todos", JSON.stringify(todos));
   };
 
-  const addTodo=(title,desc)=>{
-   console.log("I'm adding this todo:"+title,desc)
+  const addTodo = (title, desc) => {
+    console.log("I'm adding this todo:" + title, desc);
+    let sno;
+    if (todos.length == 0) {
+      sno = 0;
+    } else {
+      sno = todos[todos.length - 1].sno + 1;
+    }
+    const myTodo = {
+      sno: sno,
+      title: title,
+      desc: desc,
+    };
+    setTodos([...todos, myTodo]);
+    console.log(myTodo);
   };
-  const [todos, setTodos] = useState([
-    {
-      sno: 1,
-      title: "Go to the market",
-      desc: "You need to go to the market to get this job done",
-    },
-    {
-      sno: 2,
-      title: "Go to the mall",
-      desc: "You need to go to the mall to get this job done2",
-    },
-    {
-      sno: 3,
-      title: "Go to the ghat",
-      desc: "You need to go to the ghat to get this job done3",
-    },
-  ]);
+  const [todos, setTodos] = useState(initTodo);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
   return (
     <>
       <Header title="My Todos List" searchBar={false} />
--- src/MyComponents/Footer.js
+++ src/MyComponents/Footer.js
@@ -1,14 +1,13 @@
 import React from "react";
 
 export const Footer = () => {
- let footerStyle={
-    position:"absolute",
-    top:"100vh",
-    width:"100%",
-    // border:"2px solid red"
-  }
+//  let footerStyle={
+//     position:"relative",
+//     top:"70vh",
+//     width:"100%",
+//   }
   return (
-    <footer className="bg-dark text-light py-3" style={footerStyle}>
+    <footer className="bg-dark text-light py-3">
       <p className="text-center">Copy &copy; MyTodosList.com</p>
     </footer>
   );
